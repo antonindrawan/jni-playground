48649aeb08bff8d4e084827153dd1abf4d164ce9
Anton Indrawan <anton.indrawan@gmail.com>
2018-06-13 22:12:10 +0200

    Add travis CI

--- HelloWorldJNI/build.sh
+++ HelloWorldJNI/build.sh
@@ -1,15 +1,18 @@
 #! /usr/bin/env bash
 
-mkdir -p output
+readonly script_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+readonly output_dir=${script_dir}/output
+
+mkdir -p ${output_dir}
 
 # This is an example of raw commands to generate JNI stub.
 # They can also be invoked from CMake using add_jar to compile Java files and add_custom_command to trigger javac.
 #  $ javac -d output src/com/example/jni/HelloWorldServiceNative.java
 #  $ javah -d output -classpath bin -jni com.example.jni.HelloWorldServiceNative
 
-pushd output
+pushd ${output_dir}
   cmake -G Ninja .. && ninja
 popd
 
 # Run the example app that uses the Native jar. Note: It can be done from CMake.
-java -Djava.library.path=output/ -cp output/HelloWorldService.jar:output/HelloWorldServiceNative.jar com.example.jni.HelloWorldService
+java -Djava.library.path=${output_dir} -cp ${output_dir}/HelloWorldService.jar:${output_dir}/HelloWorldServiceNative.jar com.example.jni.HelloWorldService

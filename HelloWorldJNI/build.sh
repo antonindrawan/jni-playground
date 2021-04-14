a2d4422ac740d3bf525f14f5c359ecc8d8fb259c
Anton Indrawan <anton.indrawan@gmail.com>
2021-04-14 23:00:24 +0200

    build: javah is not supported since Java 1.10

--- HelloWorldJNI/build.sh
+++ HelloWorldJNI/build.sh
@@ -6,8 +6,12 @@ readonly output_dir=${script_dir}/output
 
 # This is an example of raw commands to generate JNI stub.
 # They can also be invoked from CMake using add_jar to compile Java files and add_custom_command to trigger javac.
+#
+# Java 1.8 & 1.9
 #  $ javac -d output src/com/example/jni/HelloWorldServiceNative.java
 #  $ javah -d output -classpath bin -jni com.example.jni.HelloWorldServiceNative
+# Java 1.10 or higher
+#  $ javac -h output src/com/example/jni/HelloWorldServiceNative.java
 
 cmake -S ${script_dir} -B ${output_dir}
 cmake --build ${output_dir}
